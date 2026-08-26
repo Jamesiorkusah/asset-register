d77464c63ed315ecd23d7f3e379b3a76d177d211
James Iorkusah <github-email@iorkusahjames23.com>
2026-08-26 13:35:38 +0100

    Add asset list and asset detail routes

--- main.py
+++ main.py
@@ -4,4 +4,26 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Asset Register is running!"}   
+    return {"message": "Asset Register is running!"}  
+
+@app.get("/assets")
+def get_assets():
+    return {"assets": [asset["type"] for asset in assets]}
+
+@app.get("/assets/{tag}")
+def get_asset_by_tag(tag: str):
+    for asset in assets:
+        if asset["tag"] == tag:
+            return {"asset": asset}
+    return {"error": "Asset not found"}
+
+assets = [
+    { "tag": "NIT-001", "type": "laptop", "model": "Dell XPS 13", "status": "in use" },
+    { "tag": "NIT-002", "type": "router", "model": "Mikrotik hEX", "status": "in use" },
+    { "tag": "NIT-003", "type": "printer", "model": "HP LaserJet Pro", "status": "faulty" },
+]
+
+import uvicorn
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)

2945271b25e0228dd385e4a65ca604d25813cafd
James Iorkusah <github-email@iorkusahjames23.com>
2026-08-28 10:50:24 +0100

    Completed today's work

--- main.py
+++ main.py
@@ -1,14 +1,20 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
+import uvicorn
 
 app = FastAPI()
+templates = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def home():
     return {"message": "Asset Register is running!"}  
 
-@app.get("/assets")
-def get_assets():
-    return {"assets": [asset["type"] for asset in assets]}
+@app.get("/assets", response_class=HTMLResponse)
+def list_assets(request: Request):
+    return templates.TemplateResponse(
+        "assets.html", {"request": request, "assets": assets}
+    )
 
 @app.get("/assets/{tag}")
 def get_asset_by_tag(tag: str):
@@ -22,8 +28,6 @@ assets = [
     { "tag": "NIT-002", "type": "router", "model": "Mikrotik hEX", "status": "in use" },
     { "tag": "NIT-003", "type": "printer", "model": "HP LaserJet Pro", "status": "faulty" },
 ]
-
-import uvicorn
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
